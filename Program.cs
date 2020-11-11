d6c0b2d2d3b2ab29fbab226816bacf5eba458c75
LameckVieira <lameck.v.barbosa@gmail.com>
2020-11-11 11:34:42 -0300

    Lógica de programação

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Logica_de_programação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // int valorUm;
+            // int valorDois;
+            
+
+
+            // Aqui vai o Código
+            //Console.WriteLine("Digite seu nome:");
+            //string nomeCompleto = Console.ReadLine();
+
+            //Console.WriteLine("bem vindo "+nomeCompleto);
+
+
+
+            // Entrada de dados
+            
+            Console.WriteLine("Soma de dois números ");
+            Console.WriteLine("--------------------");
+            Console.Write("Digite o primeiro valor: ");
+            int valorUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            int valorDois = int.Parse(Console.ReadLine());
+
+            //Porcessamento
+
+            int soma = valorUm + valorDois;
+
+            //Exibir
+
+            Console.WriteLine("A soma de "+valorUm+" + "+valorDois+" = "+soma);
+            
+        }
+    }
+}
